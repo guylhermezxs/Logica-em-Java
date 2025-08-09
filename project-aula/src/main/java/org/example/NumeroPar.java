c537c3f0b844ac819342071d2b377d959615a2a6
Guilherme <guifefo@gmail.com>
2025-08-08 21:38:10 -0300

    Add files via upload

---
+++ project-aula/src/main/java/org/example/NumeroPar.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NumeroPar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 100; i <=120; i++){
+
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+
+        }
+
+
+    }
+}
